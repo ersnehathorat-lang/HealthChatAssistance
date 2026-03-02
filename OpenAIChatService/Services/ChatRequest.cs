c42de46f8eda423fa88bcd014e2db79af4d1841d
Sneha T <er.snehathorat@gmail.com>
2026-03-02 18:18:42 +0000

    Add namespace to ChatRequest class

--- OpenAIChatService/Services/ChatRequest.cs
+++ OpenAIChatService/Services/ChatRequest.cs
@@ -1,4 +1,7 @@
- public class ChatRequest
-    {
-        public string Prompt { get; set; }
-    }
+﻿namespace OpenAIChatService.Services
+{
+   public class ChatRequest
+       {
+           public string Prompt { get; set; }
+       }
+ }
